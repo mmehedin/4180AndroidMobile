4b191f6c737190c215551eefb2a42479fec6cc1e
Mihai <Mihai@Adinas-iMac.local>
2017-01-20 13:32:19 -0500

    HW1

---
+++ HW1_Mehedint/app/src/main/java/com/example/mihai/hw1_mehedint/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mihai.hw1_mehedint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
